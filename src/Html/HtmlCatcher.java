7e5fb1fa09fb4c07a282d0c619a67a43a21c89cf
jianjian <jianjian198710@hotmail.com>
2013-08-22 23:22:55 +0800

    Catch Html

--- src/Html/HtmlCatcher.java
+++ src/Html/HtmlCatcher.java
@@ -3,27 +3,60 @@ package Html;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 public class HtmlCatcher {
-	private static boolean useProxy;
+	private static boolean useProxy = false;
+	private static DefaultHttpClient httpClient;
+	
+	public static DefaultHttpClient getHttpClientInstance(){
+		if(httpClient == null){
+			httpClient = new DefaultHttpClient();
+		}
+		return httpClient;
+	}
 	
 	public static String catchHtml(String url){
 		StringBuffer html = new StringBuffer();
 		//创建HttpClient对象
-		HttpClient httpClient = new DefaultHttpClient();
-		httpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, new HttpHost("www-proxy.ao.ericsson.se", 8080, "http"));
-
+		httpClient = getHttpClientInstance();
+		//使用Ericsson Proxy
+		if(isUseProxy()==true){
+			httpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, new HttpHost("www-proxy.ao.ericsson.se", 8080, "http"));
+		}
 		//以Get方式请求URL
 		HttpGet httpGet = new HttpGet(url);
+         
+//        List <NameValuePair> formparams = new ArrayList<NameValuePair>();
+//        formparams.add(new BasicNameValuePair("username", "jianjian198710@hotmail.com"));
+//        formparams.add(new BasicNameValuePair("password", "198710"));
+//        HttpPost httpPost = new HttpPost(url); 
+//        httpPost.setEntity(new UrlEncodedFormEntity(formparams, Consts.UTF_8)); 
+//        try {
+//			System.out.println(EntityUtils.toString(httpPost.getEntity()));
+//		} catch (ParseException | IOException e) {
+//			e.printStackTrace();
+//		}
+//        
 		try{
+//			HttpResponse response = httpClient.execute(httpPost);
 			HttpResponse response = httpClient.execute(httpGet);
 			//获取状态码
 			int resStatus = response.getStatusLine().getStatusCode();
@@ -50,7 +83,16 @@ public class HtmlCatcher {
 		return html.toString();
 	}
 	
+	public static boolean isUseProxy() {
+		return useProxy;
+	}
+
+	public static void setUseProxy(boolean useProxy) {
+		HtmlCatcher.useProxy = useProxy;
+	}
+
 	public static void main(String[] args){
-		System.out.println(catchHtml("http://www.baidu.com"));
+		setUseProxy(false);
+		System.out.println(catchHtml("http://ikandou.com/accounts/login/?next=/g/"));
 	}
 }
