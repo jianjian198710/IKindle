5dd6c8c311bb22e90ebcd09a72be4322f1d25231
unknown <eybcdgr@E7E4115B4473DC.eapac.ericsson.se>
2013-08-22 18:00:39 +0800

    Catch Html

--- src/Download/DownloadUtil.java
+++ src/Download/DownloadUtil.java
@@ -1,5 +1,8 @@
 package Download;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class DownloadUtil {
 
 	//资源的路径
@@ -20,12 +23,12 @@ public class DownloadUtil {
 		threads = new DownThread[threadNum];
 	}
 
-	public void download(){
+	public void download() throws MalformedURLException{
 		URL url = new URL(path);
 	}
 	
 	public double getCompeleteRate(){
-		
+		return 0.00;
 	}
 	
 	private class DownThread extends Thread{
---
+++ src/Html/HtmlCatcher.java
@@ -0,0 +1,56 @@
+package Html;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.impl.client.DefaultHttpClient;
+public class HtmlCatcher {
+	private static boolean useProxy;
+	
+	public static String catchHtml(String url){
+		StringBuffer html = new StringBuffer();
+		//创建HttpClient对象
+		HttpClient httpClient = new DefaultHttpClient();
+		httpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, new HttpHost("www-proxy.ao.ericsson.se", 8080, "http"));
+
+		//以Get方式请求URL
+		HttpGet httpGet = new HttpGet(url);
+		try{
+			HttpResponse response = httpClient.execute(httpGet);
+			//获取状态码
+			int resStatus = response.getStatusLine().getStatusCode();
+			System.out.println(resStatus);
+			//200 ok
+			if(resStatus==HttpStatus.SC_OK){
+				HttpEntity entity = response.getEntity();
+				if(entity!=null){
+					try(BufferedReader br = new BufferedReader(new InputStreamReader(entity.getContent()))){
+						String s = null;
+						while((s=br.readLine())!=null){
+							html.append(s);
+						}
+					}catch(IOException ioe){
+						ioe.printStackTrace();
+					}
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("访问"+url+"出错");
+		}finally {  
+	        httpClient.getConnectionManager().shutdown();  
+	    }
+		return html.toString();
+	}
+	
+	public static void main(String[] args){
+		System.out.println(catchHtml("http://www.baidu.com"));
+	}
+}
